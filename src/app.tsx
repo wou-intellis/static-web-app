cf7656440031e546888663adb7f4ea297da23dad
Wouter van Vliet <wou@intellis.dk>
2025-03-04 13:35:48 +0100

    Initial app

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+export function App() {
+    return <>
+        <title>Static web app</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <main>
+            <p>This is the body</p>
+        </main>
+    </>
+}
